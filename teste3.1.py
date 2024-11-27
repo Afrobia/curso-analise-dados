8d7125508745c785b9ffd997ee10cfe3402a6973
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-11-27 16:36:31 -0300

    aula 3.1 adicionando arq py

---
+++ teste3.1.py
@@ -0,0 +1,11 @@
+tempo = "dia"
+
+if tempo == "dia":
+	print("Bom dia")
+
+nome = str(input("Qual seu nome?"))
+print("Olá, ", nome)
+
+num = int(input("Digite um número: "))
+print("O número digitado foi: ", num)
+print("O dobro do número digitado é: ", num*2)
